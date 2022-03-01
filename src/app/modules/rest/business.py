c59cb7a649359a4f4994d033818a8d15f0cc08c1
Furkan Kalkan <furkankalkan@mantis.com.tr>
2022-03-01 17:18:11 +0300

    Base64 format changed to urlsafe

--- src/app/modules/rest/business.py
+++ src/app/modules/rest/business.py
@@ -20,7 +20,7 @@ def encrypt_text(args):
     text = args.get('text')
     try:
         text = pad(text.encode('utf-8'), AES.block_size)
-        text = base64.b64encode(_get_aes_cipher().encrypt(text))
+        text = base64.urlsafe_b64encode(_get_aes_cipher().encrypt(text))
     except Exception as e:
         tb = traceback.format_exc()
         return abort(http_status_code=HTTPStatus.BAD_REQUEST, message="An error occurred while encrypting text",
@@ -31,7 +31,7 @@ def encrypt_text(args):
 def decrypt_text(args):
     text = args.get('text')
     try:
-        text = base64.b64decode(f"{text}==")
+        text = base64.urlsafe_b64decode(f"{text}==")
         text = unpad(_get_aes_cipher().decrypt(text), AES.block_size)
     except Exception as e:
         tb = traceback.format_exc()
